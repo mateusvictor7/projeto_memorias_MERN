9d5a05ca8d440f4741b5d14ec591b25065f3505a
Mateus Santiago <mateussantiago.victor@gmail.com>
2022-03-03 01:35:17 -0300

    Resolvendo conflitos

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -20,7 +20,7 @@ const createPost = async (req, res) => {
 
     res.status(201).json(newPost);
   } catch (error) {
-    res.status(409).json({ message: error.message })
+    res.status(409).json({ message: error.message });
   }
 }
 
