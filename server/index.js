fd26ca5af955c061365e8c0cd2002dcbaf0b5828
Mateus Santiago <72159328+mateusvictor7@users.noreply.github.com>
2022-03-03 00:37:02 -0300

    Revert "Setup do Mongo e do index feito"

--- server/index.js
+++ server/index.js
@@ -2,21 +2,3 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
-const app = express();
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
-app.use(cors());
-
-const CONNECTION_URL = "mongodb+srv://mateusvictor7:mateus0723@cluster0.cyxd1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-
-const PORT = process.env.PORT || 5000;
-
-mongoose.connect(CONNECTION_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => app.listen(PORT, () => console.log(`Aplicação rodando na porta ${PORT}`)))
-  .catch((error) => console.log(error.message));
