eaac8794200cac563c8265c50fc53e787cb66887
Mateus Santiago <mateussantiago.victor@gmail.com>
2022-03-04 16:15:37 -0300

    estruturando o front e adicionando estilo

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import useStyles from './styles';
+
+const Post = () => {
+  const classes = useStyles();
+
+  return (
+    <h1>Posts</h1>
+  )
+}
+
+export default Post;
