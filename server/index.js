ae56d0bcfaa8a9c1ad7ae6cbb9f0c9b93b3d5d01
Mateus Santiago <mateussantiago.victor@gmail.com>
2022-03-04 18:31:58 -0300

    Implementando redux

--- server/index.js
+++ server/index.js
@@ -8,13 +8,13 @@ const postRoutes = require('./routes/posts');
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
 
+app.use('/posts', postRoutes);
 const CONNECTION_URL = "mongodb+srv://mateusvictor7:mateus0723@cluster0.cyxd1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 const PORT = process.env.PORT || 5000;
